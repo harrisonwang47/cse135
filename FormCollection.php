60dba3f71e8e81a0b15848a4272f317d28869d84
Harrison Wang <haw046@ucsd.edu>
2016-08-14 22:12:09 -0700

    form action

--- FormCollection.php
+++ FormCollection.php
@@ -1,39 +1,36 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Form Collection PHP</title>
-</head>
-<body>
+	<head>
+		<meta charset="utf-8">
+		<title>Form Collection PHP</title>
+	</head>
 	
-<h1>Form Collection</h1>
-
-<form action="echo.php" method="get">
-
-<label>First Name:
- <input type="text" name="firstName" id="firstName">
-</label>
-
-<label>Last Name:
- <input type="text" name="lasttName" id="lastName">
-</label>
-
-<label for="formAction">Method:</label>
- <select id="formAction" onchange="this.form.method = this.options[selectedIndex].value;">
-	<option>GET</option>
-	<option>POST</option>
- </select>
-
-<label for="formAction">Color:</label>
- <select id="formAction" onchange="this.form.method = this.options[selectedIndex].value;">
-	<option>RED</option>
-	<option>WHITE</option>
-	<option>BLUE</option>
- </select>
-
-<input type="submit" value="Submit">
-
-</form>
-
-</body>
+	<body>
+		<h1>Form Collection</h1>
+		
+		<form action="echo.php" method="get">
+			<label>First Name:
+			 <input type="text" name="firstName" id="firstName">
+			</label>
+			
+			<label>Last Name:
+			 <input type="text" name="lasttName" id="lastName">
+			</label>
+			
+			<label for="formAction">Method:</label>
+			 <select id="formAction" onchange="this.form.method = this.options[selectedIndex].value;">
+				<option>GET</option>
+				<option>POST</option>
+			 </select>
+			
+			<label for="formAction">Color:</label>
+			 <select id="formAction" onchange="this.form.method = this.options[selectedIndex].value;">
+				<option>RED</option>
+				<option>WHITE</option>
+				<option>BLUE</option>
+			 </select>
+			
+			<input type="submit" value="Submit">
+		</form>
+	</body>
 </html>
---
+++ echo.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>ECHO</title>
+	</head>
+	
+	<body>	
+		<h1>Form Collection</h1>
+		<p> Hello <?php echo $_POST["firstName"]; ?> from a Web app written in PHP on </p>
+		 <?php	
+		 	date_default_timezone_set("America/Los_Angeles");
+		  	echo date('m/d/Y, ');
+		  	echo '<script type="text/javascript">
+					var currentTime = new Date();
+					var hours = currentTime.getHours();
+					var minutes = currentTime.getMinutes();
+	
+					var suffix = "AM";
+	
+					if (hours >= 12) {
+					    suffix = "PM";
+					    hours = hours - 12;
+					}
+					
+					if (hours == 0) {
+					    hours = 12;
+					}
+					
+					if (minutes < 10) {
+					    minutes = "0" + minutes;
+					}
+					
+					document.write("<b>" + hours + ":" + minutes + " " + suffix + "</b>");
+				</script>';
+		?> 
+	</body>
+</html>
