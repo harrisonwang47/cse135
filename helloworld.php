2b6496f780636c28702557933b7fe0191485ebb0
Harrison Wang <haw046@ucsd.edu>
2016-08-14 17:54:20 -0700

    local time

--- helloworld.php
+++ helloworld.php
@@ -9,7 +9,7 @@
 		<?php 
   		  date_default_timezone_set("America/Los Angeles");
 		  print "Hello Web World from Language PHP on "; 
-		  echo(date('m-d-Y h:i:s A'));
+		  echo(date('m-d-Y H:i:s T'));
 		?>
 	</body>
 </html>
