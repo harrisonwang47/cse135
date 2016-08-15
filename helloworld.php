997d425866a658e3efc196734db51ef4b9fd1e84
Harrison Wang <haw046@ucsd.edu>
2016-08-14 18:04:18 -0700

    localtime

--- helloworld.php
+++ helloworld.php
@@ -7,9 +7,31 @@
 
 	<body>
 		<?php 
-  		  date_default_timezone_set("America/Los Angeles");
 		  print "Hello Web World from Language PHP on "; 
-		  echo date('m/d/Y, H:i:s');
+		  #date_default_timezone_set("America/Los Angeles");
+		  #echo date('m/d/Y, H:i:s');
+		  echo '<script type="text/javascript">
+					var currentTime = new Date();
+					var hours = currentTime.getHours();
+					var minutes = currentTime.getMinutes();
+	
+					var suffix = "AM";
+	
+					if (hours >= 12) {
+					    suffix = "PM";
+					    hours = hours - 12;
+					}
+					
+					if (hours == 0) {
+					    hours = 12;
+					}
+					
+					if (minutes < 10) {
+					    minutes = "0" + minutes;
+					}
+					
+					document.write("<b>" + hours + ":" + minutes + " " + suffix + "</b>");
+				</script>';
 		?>
 	</body>
 </html>
