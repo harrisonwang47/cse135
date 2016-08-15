ee1fa355ad6a2960e2154cdb12760d5a7b1abf8f
Harrison Wang <haw046@ucsd.edu>
2016-08-14 18:13:51 -0700

    background colors

--- helloworld.php
+++ helloworld.php
@@ -3,15 +3,17 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html" />
 		<title>Hello World PHP Style</title>
+		<style>
+			#topbar { border-bottom:4px solid #<?php echo $r.$g.$b; ?>; }
+		</style>
 	</head>
 
 	<body>
 		<?php 
-		  print "Hello Web World from Language PHP on "; 
-		  date_default_timezone_set("America/Los Angeles");
-		  echo date('m/d/Y, ');
-		  #echo <br>;
-		  echo '<script type="text/javascript">
+		  	print "Hello Web World from Language PHP on "; 
+		  	date_default_timezone_set("America/Los Angeles");
+		  	echo date('m/d/Y, ');
+		  	echo '<script type="text/javascript">
 					var currentTime = new Date();
 					var hours = currentTime.getHours();
 					var minutes = currentTime.getMinutes();
@@ -33,6 +35,11 @@
 					
 					document.write("<b>" + hours + ":" + minutes + " " + suffix + "</b>");
 				</script>';
+		 	
+		 	$randomString = md5(time().rand(0,999)); 
+			$r = substr($randomString,0,2); //1. and 2.
+			$g = substr($randomString,2,2); //3. and 4.
+			$b = substr($randomString,4,2); //5. and 6.
 		?>
 	</body>
 </html>
