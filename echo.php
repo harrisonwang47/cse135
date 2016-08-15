fc1ab250a2b729191b9c80e9c9f7f5cf050fe27c
Harrison Wang <haw046@ucsd.edu>
2016-08-14 22:45:37 -0700

    form collection

--- echo.php
+++ echo.php
@@ -7,7 +7,8 @@
 	
 	<body>	
 		<h1>Form Collection</h1>
-		<p> Hello <?php echo $_POST["firstName"];?> <?php echo $_POST["lastName"];?> from a Web app written in PHP on 
+		<p> Hello <?php echo $_POST["firstName"];?> <?php echo $_POST["lastName"];?> 
+				  <?php echo $_GET["firstName"];?> <?php echo $_GET["lastName"];?> from a Web app written in PHP on 
 		 <?php	
 		 	date_default_timezone_set("America/Los_Angeles");
 		  	echo date('m/d/Y, ');
