7d9f48e43bb247f2d335d3c937c9145879969195
Harrison Wang <haw046@ucsd.edu>
2016-08-14 20:47:02 -0700

    environment vars

--- eve.php
+++ eve.php
@@ -7,6 +7,10 @@
 
 	<body>
 		<p>This program will print out all the environment variables from the request and related values. </p>
-		<?php echo '<pre>' . var_dump($_ENV) . '</pre>';?>
+		<?php 
+		var_dump($_GET);
+		var_dump($_POST);
+		var_dump($_REQUEST);
+		?>
 	</body>
 </html>
