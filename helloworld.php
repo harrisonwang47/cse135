0b515c82096b0048ec901294589e58df6d90218b
Harrison Wang <haw046@ucsd.edu>
2016-08-14 17:32:16 -0700

    test

--- helloworld.php
+++ helloworld.php
@@ -7,9 +7,9 @@
 
 	<body>
 		<?php 
-		  $localtime = new DateTime("now", new DateTimeZone('America/New York'));
-		  print "Hello Web World from Language PHP on" 
-		  echo $localtime ->format('m/d/Y, H:i:s');
+		  #$localtime = new DateTime("now", new DateTimeZone('America/New York'));
+		  print "Hello Web World from Language PHP on" print_r(localtime());
+		  #echo $localtime ->format('m/d/Y, H:i:s');
 		?>
 	</body>
 </html>
