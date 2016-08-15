1d62ec5cdae79381ab116a4cc2b94c8492393a35
Harrison Wang <haw046@ucsd.edu>
2016-08-14 18:20:00 -0700

    background colors

--- helloworld.php
+++ helloworld.php
@@ -35,6 +35,7 @@
 		 	
 		 	$background_colors = array('#282E33', '#25373A', '#164852', '#495E67', '#FF3838');
 			$rand_background = $background_colors[array_rand($background_colors)];
+			echo $rand_background;
 		?>
 	</body>
 </html>
