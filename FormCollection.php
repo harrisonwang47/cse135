c8dc937fb5dfa1a72cb6db024356d0e6d47e8bbc
Harrison Wang <haw046@ucsd.edu>
2016-08-14 21:46:24 -0700

    FormCollection

---
+++ FormCollection.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Form Collection PHP</title>
+</head>
+<body>
+	
+<h1>Form Collection</h1>
+
+<form action="echo" method="get">
+
+<label>First Name:
+ <input type="text" name="firstName" id="firstName">
+</label>
+
+<label for="formAction">Form Action:</label>
+ <select id="formAction" onchange="this.form.method = this.options[selectedIndex].value;">
+	<option>GET</option>
+	<option>POST</option>
+ </select>
+
+
+<input type="submit" value="Submit">
+
+</form>
+
+</body>
+</html>
