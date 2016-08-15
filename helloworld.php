d1342c13962ca0217c3974c386cba1604f19dda4
Harrison Wang <haw046@ucsd.edu>
2016-08-14 17:30:43 -0700

    test

--- helloworld.php
+++ helloworld.php
@@ -1,15 +1,15 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=iso- 8859-1" />
+		<meta http-equiv="Content-Type" content="text/html" />
 		<title>Hello World PHP Style</title>
 	</head>
 
 	<body>
-		<?php $localtime = new DateTime("now", new DateTimeZone('America/New York'));
-		  #print "Hello Web World from Language PHP on" 
-		  echo $localtime -> format('m/d/Y, H:i:s');
+		<?php 
+		  #$localtime = new DateTime("now", new DateTimeZone('America/New York'));
+		  print "Hello Web World from Language PHP on" 
+		  #echo $localtime ->format('m/d/Y, H:i:s');
 		?>
 	</body>
-
 </html>
