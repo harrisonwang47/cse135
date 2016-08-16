337404924c6176e2829c4c45f3a4d721d597876e
Harrison Wang <haw046@ucsd.edu>
2016-08-15 20:20:25 -0700

    add helloworld.cgi

--- helloworld.php
+++ helloworld.php
@@ -10,6 +10,7 @@
 		  	print "Hello Web World from Language PHP on "; 
 		  	date_default_timezone_set("America/Los_Angeles");
 		  	echo date('m/d/Y, ');
+		  	#gets the local time
 		  	echo '<script type="text/javascript">
 					var currentTime = new Date();
 					var hours = currentTime.getHours();
