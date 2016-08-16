fcc6e321d9666b5771dbb277d26da384c17d1b16
Harrison Wang <haw046@ucsd.edu>
2016-08-16 16:50:09 -0700

    edit sessionpage2

--- sessionpage2.php
+++ sessionpage2.php
@@ -5,30 +5,29 @@
 	</head>
 	
 	<body>
-			<?php
-				session_start();
-				
-				echo <script>
-				if($_SESSION['username'])
-				{
-					echo "Hi" . $_SESSION['username'] . "nice to meet you!";
-				}
-				else
-				{
-					echo "Howdy stranger...tell me your name on page1!<br>";
-					echo " "
-					echo "<a href = \"sessionpage1.php\"> Go back to SessionPage1</a>";
-				}
-
-				if(isset($_POST['delete']))
-				{
-					unset($_SESSION[‘username’]);
-				}
-				</script>
-			?>
-
-			<form>
-				<button type="submit" name="delete">Clear Session</button>
-			</form>
+		<?php
+			session_start();
+			
+			#check to see if the session variable is set
+			if($_SESSION['username'])
+			{
+				echo "Hi" . $_SESSION['username'] . "nice to meet you!";
+			}
+			else
+			{
+				echo "Howdy stranger...tell me your name on page1!<br>";
+				echo " "
+				echo "<a href = \"sessionpage1.php\"> Go back to SessionPage1</a>";
+			}
+			
+			#delete the session variable
+			if(isset($_POST['delete']))
+			{
+				unset($_SESSION[‘username’]);
+			}
+		?>
+		<form>
+			<button type="submit" name="delete">Clear Session</button>
+		</form>
 	</body>
 </html>
