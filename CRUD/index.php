2f6157aab3a6f0565d963ec01652a5827eb67541
Harrison Wang <haw046@ucsd.edu>
2016-08-24 20:03:09 -0700

    CRUD db connect

--- CRUD/index.php
+++ CRUD/index.php
@@ -17,7 +17,7 @@
 
 	// PLEASE CHANGE THESE LINES!!!!!!
     define('DB_USER','harrison');
-    define('DB_PASSWORD','password');
+    define('DB_PASSWORD','Team135tk!');
     define('DB_HOST','127.0.0.1');
     define('DB_NAME','userDB');
 
