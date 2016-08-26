550e638936e88bd8802e42e72a7f537bb7fb45ec
Harrison Wang <haw046@ucsd.edu>
2016-08-26 11:35:37 -0700

    edit update

--- BoxOffice/action.php
+++ BoxOffice/action.php
@@ -13,6 +13,7 @@
 	   $box_office = $_REQUEST['box_office'];
 	   
 	   // SHOULD HAVE VALIDATION HERE!?
+	   //if there are wrong inputs, show error
 		
 	
 	   $sql = "INSERT INTO movies (movie_title,studio,year,box_office) VALUES ('$movie_title' , '$studio' , '$year' , '$box_office')";
@@ -27,7 +28,7 @@
 	   $box_office = $_REQUEST['box_office'];
 	   $user_id = $_REQUEST['user_id'];
 	
-	   $sql = "UPDATE movies SET movie_title='" .$movie_title."' ,studio='".$studio."' ,year='".$year."' ,box_office='".$box_office."' WHERE user_id='".$user_id."'";
+	   $sql = "UPDATE movies SET movie_title='" .$movie_title."' ,studio='".$studio."' ,year='".$year."' ,box_office='".$box_office."' WHERE user_id='".$movie_id."'";
        $result = mysqli_query($conn, $sql);
 		
 	}  else if ($action == "Delete") {
